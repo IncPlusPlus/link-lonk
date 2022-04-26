2d4b98bcc9b84fdc03f49dc21daad85df519837b
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-04-26 10:50:42 -0400

    Log out when the process is ending

--- src/Bot.ts
+++ src/Bot.ts
@@ -12,3 +12,15 @@ ready(client);
 
 client.login(token);
 console.log(client);
+
+const handleShutdown = (...args: any[]) => {
+    console.log(`link-lonk received ${args[0]}`);
+    client.destroy();
+    console.log('Client destroyed.');
+    process.exit();
+}
+
+// Handle shutdown
+process.on('SIGINT', handleShutdown);
+process.on('SIGTERM', handleShutdown);
+process.on('SIGKILL', handleShutdown);
