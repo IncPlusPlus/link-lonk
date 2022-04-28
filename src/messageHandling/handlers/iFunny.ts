59a51c14915bd40394c81b82b1cd45702b7e4216
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-04-27 20:45:15 -0400

    Support all iFunny media + added error handling
    Now this can handle pictures and GIFs too. Also, if the attachments are too large to upload, we reply with direct links instead.

--- src/messageHandling/handlers/iFunny.ts
+++ src/messageHandling/handlers/iFunny.ts
@@ -1,7 +1,6 @@
-import {Client, Message, MessageAttachment} from "discord.js";
+import {Client, DiscordAPIError, Message, MessageAttachment} from "discord.js";
 import cheerio from 'cheerio';
 import got from 'got';
-import {CheerioAPI} from "cheerio/lib/load";
 
 const iFunnyVideoRegEx = new RegExp('https:\\/\\/ifunny.co\\/(video|picture|gif)\\/\\w+', 'g');
 
@@ -34,8 +33,22 @@ export const handleIFunnyVideo = (client: Client, message: Message): boolean => 
             message.reply({
                 files: files,
             })
-                .then(() => console.log(`Replied to message "${message.id}" with content "${message.content}". Suppressing embeds for that message...`))
-                .catch((replyFailReason) => console.log(`Failed to reply to message "${message.id}" with content "${message.content}". Reason: ${replyFailReason}`));
+                .then(() => console.log(`Replied to message "${message.id}" with content "${message.content}".`))
+                .catch((replyFailReason) => {
+                    // Check if this is an error we recognize
+                    if (replyFailReason instanceof DiscordAPIError) {
+                        // Check if the reason the reply failed is that the file was too large
+                        if (replyFailReason.httpStatus === 413) {
+                            // Try just linking the file instead of uploading it.
+                            console.log(`Failed to reply to message  "${message.id}" with content "${message.content}" because the file(s) were too big. Trying again by just sending the links to the file(s)...`);
+                            let mediaLinks = "Here are the file(s) of the media you linked to as hyperlinks. Seems I couldn't upload them here because they were too big.";
+                            files.forEach(file => mediaLinks += " " + file.attachment);
+                            message.reply(mediaLinks);
+                        }
+                    }
+                })
+                // Well at least we tried!
+                .catch((secondFailReason) => console.log(`Failed to reply to message "${message.id}" with content "${message.content}". Reason: ${secondFailReason}`));
         });
 
     return true;
@@ -61,26 +74,44 @@ const scrapeIFunny = async (pageUrl: string, mediaType: string): Promise<{ media
         return {mediaUrl: "", mediaFileName: "INVALID NAME"};
     }
     const pageBody$ = cheerio.load(response.body);
+    const selector = pageBody$(getCSSSelectorForIFunny(mediaType));
+    if (selector.length !== 1) {
+        throw new Error(`Expected to find 1 ${mediaType} pane on the iFunny ${mediaType} page but found ${selector.length}`);
+    }
+    const meme = selector[0];
+    let mediaUrl;
     switch (mediaType) {
         case 'video':
-            return scrapeIFunnyForVideo(pageBody$);
+            // @ts-ignore YES IT DOES HAVE THAT ATTRIBUTE, STUPID!
+            mediaUrl = meme.attribs["data-src"];
+            break;
+        case 'picture':
+            // @ts-ignore YES IT DOES HAVE THAT ATTRIBUTE, STUPID!
+            mediaUrl = meme.attribs["src"];
+            break;
+        case 'gif':
+            // @ts-ignore YES IT DOES HAVE THAT ATTRIBUTE, STUPID!
+            mediaUrl = meme.attribs["data-src"];
+            break;
         default:
             console.log(`Couldn't process iFunny link '${pageUrl} with unknown media type '${mediaType}'.`);
             return {mediaUrl: "", mediaFileName: "INVALID MEDIA TYPE"};
     }
+    return {mediaUrl, mediaFileName: mediaUrl.substring(mediaUrl.lastIndexOf("/") + 1)};
 }
 
-const scrapeIFunnyForVideo = (pageBody$: CheerioAPI): { mediaUrl: string; mediaFileName: string } => {
-// Look for CSS selector for the video pane
-    const selector = pageBody$('#App > div.v9ev > div.xbey > div > div > div._3ZEF > div > video');
-    if (selector.length !== 1) {
-        throw new Error(`Expected to find 1 video pane on the iFunny video page but found ${selector.length}`);
+const getCSSSelectorForIFunny = (mediaType: string): string => {
+    switch (mediaType) {
+        case 'video':
+            return '#App > div.v9ev > div.xbey > div > div > div._3ZEF > div > video';
+        case 'picture':
+            return '#App > div.v9ev > div.xbey > div > div > div._3ZEF > img';
+        case 'gif':
+            return '#App > div.v9ev > div.xbey > div > div > div._3ZEF > img';
+        default:
+            console.log(`Unknown iFunny media type '${mediaType}'.`)
+            return '';
     }
-    const meme = selector[0];
-    const videoUrl = meme.attribs["data-src"];
-    // const thumbnailUrl = meme.attribs["data-poster"];
-    // const title = pageBody$('#App > div.v9ev > div.xbey > div > div > h1').text();
-    return {mediaUrl: videoUrl, mediaFileName: videoUrl.substring(videoUrl.lastIndexOf("/") + 1)};
 }
 
 // const createEmbedForIFunny=(pageUrl:string,title:string, thumbnailUrl:string, videoUrl:string): MessageEmbed => {
